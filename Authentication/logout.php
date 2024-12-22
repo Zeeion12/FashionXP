7fb786fbf1e3638ec0cbec0169e570cde9ea848f
Zeeion12 <email.tioaganis96@gmail.com>
2024-12-22 22:16:32 +0700

    Ulang

--- Authentication/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua sesi
-session_destroy(); // Menghancurkan sesi
-header("Location: sign-in.php"); // Arahkan kembali ke halaman login
-exit();
-?>
